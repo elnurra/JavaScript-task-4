08e9500d44d8d51d61b0eec1ce48bc654ab49b97
elnurra <elnurra@code.edu.az>
2022-10-13 21:22:20 +0400

    Task completed

--- JS/index.js
+++ JS/index.js
@@ -5,19 +5,30 @@ const label2 = document.querySelector(".surname-error");
 const label3 = document.querySelector(".gmail-error");
 btn.addEventListener("click", () => {
   const fname = document.getElementById("fname").value;
+  let condition = false;
+  let condition2 = false;
+  let condition3 = false;
   const lname = document.getElementById("lname").value;
   const email = document.getElementById("gmail").value;
-  if (fname === "") {
-    label.innerHTML = "<br/><i>Empty input FirstName <i>";
+  if (fname.trim().length === 0) {
+    label.innerHTML = "<i>Empty input FirstName <i>";
   } else {
-    if (lname === "") {
-      label2.innerHTML = "<br><i>Empty input LastName<i>";
-    } else {
-      if (email === "") {
-        label3.innerHTML = "<br><i>Empty input Gmail<i>";
-      } else {
-        newDiv.innerHTML = `<br />WELCOME!<br />Name:${fname}<br />Lastname:${lname}<br />Email: ${email}`;
-      }
-    }
+    condition = true;
+  }
+  if (lname.trim().length === 0) {
+    label2.innerHTML = "<i>Empty input LastName<i>";
+  } else {
+    condition2 = true;
+  }
+  if (email.trim().length === 0) {
+    label3.innerHTML = "<i>Empty input Gmail<i>";
+  } else {
+    condition3 = true;
+  }
+  if (condition && condition2 && condition3) {
+    newDiv.innerHTML = `<br />WELCOME!<br />Name:${fname}<br />Lastname:${lname}<br />Email: ${email}`;
+    label.innerHTML = "<b>Correct</b> <br />";
+    label2.innerHTML = "<b>Correct</b> <br />";
+    label3.innerHTML = "<b>Correct</b> <br />";
   }
 });
