9294d18c46c2ede912bcb6662b222beedead0ae9
elnurra <elnurra@code.edu.az>
2022-10-13 19:02:18 +0400

    JavaScript Form fully completed

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,14 @@
 const btn = document.querySelector(".apply button");
 const newDiv = document.querySelector(".submit");
-const label = document.querySelector(".fname");
-const label2 = document.querySelector(".lname");
-const label3 = document.querySelector(".gmail");
+const label = document.querySelector(".name-error");
+const label2 = document.querySelector(".surname-error");
+const label3 = document.querySelector(".gmail-error");
 btn.addEventListener("click", () => {
   const fname = document.getElementById("fname").value;
   const lname = document.getElementById("lname").value;
   const email = document.getElementById("gmail").value;
   if (fname === "") {
     label.innerHTML = "<br/><i>Empty input FirstName <i>";
-    label.innerHTML.length = 0;
   } else {
     if (lname === "") {
       label2.innerHTML = "<br><i>Empty input LastName<i>";
